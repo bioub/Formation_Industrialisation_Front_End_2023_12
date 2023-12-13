f894abaaf2c34eed2c60cd8946f4c606fadfba47
Romain Bohdanowicz <romain.bohdanowicz@formation.tech>
2023-12-13 11:25:02 +0100

    Correction Webpack

--- ex-todos/webpack.config.js
+++ ex-todos/webpack.config.js
@@ -1,28 +1,30 @@
-const HtmlWebpackPlugin = require('html-webpack-plugin');
-const webpack = require('webpack');
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const webpack = require("webpack");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 
 /** @type {import('webpack').Configuration} */
 const config = {
-  devtool: 'source-map',
-  entry: './src/main.js',
+  devtool: "source-map",
+  entry: "./src/main.js",
   output: {
     clean: true,
-    filename: 'bundle.js'
+    filename: "bundle.js",
   },
   plugins: [
     new HtmlWebpackPlugin({
-    template: './index.html'
-  }),
-  new webpack.BannerPlugin('Copyright MyCompany')
-],
+      template: "./index.html",
+    }),
+    new webpack.BannerPlugin("Copyright MyCompany"),
+    new MiniCssExtractPlugin({ filename: "bundle.css" }),
+  ],
   module: {
     rules: [
       {
         test: /\.scss/,
-        use: ['style-loader', 'css-loader', 'sass-loader']
-      }
-    ]
-  }
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
+  },
 };
 
 module.exports = config;
